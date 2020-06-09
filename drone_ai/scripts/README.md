d301835afda0091dac1c33351cc2884d5db81842
Alessandro Freitas <alessandro.spiercort@hotmail.com>
2020-06-08 22:29:22 -0300

    view only camera tello

--- drone_ai/scripts/README.md
+++ drone_ai/scripts/README.md
@@ -1,3 +1,10 @@
+#### Versão usada do Python
+3.6.9
+
 #### Para verificar o status do Tello, executar o seguinte comando:
 
 python3 tello_status.py
+
+#### Verificar a camera do drone
+
+python3 camera.py
--- drone_ai/scripts/camera.py
+++ drone_ai/scripts/camera.py
@@ -1,18 +1,21 @@
 from djitellopy import Tello
 import cv2, math, time
 
+# Inicia o SDK do Tello
 tello = Tello()
+
 if not tello.connect():
   print("Cannot reach tello, aborting!")
   exit(1)
 
 tello.streamoff()
-tello.takeoff()
 time.sleep(1)
-
 tello.streamon()
 
-cv2.namedWindow("drone")
+# Nível da bateria do Tello
+battery = tello.query_battery()
+
+cv2.namedWindow("Tello")
 frame_read = tello.get_frame_read()
 
 while True:
@@ -21,15 +24,6 @@ while True:
 
   key = cv2.waitKey(1) & 0xff
   if key == ord('q'):
-    tello.land()
     frame_read.stop()
     tello.streamoff()
     exit(0)
-  elif key == ord('w'):
-    tello.move_up(30)
-  elif key == ord('s'):
-    tello.move_down(30)
-  elif key == ord('a'):
-    tello.rotate_counter_clockwise(20)
-  elif key == ord('d'):
-    tello.rotate_clockwise(20)
